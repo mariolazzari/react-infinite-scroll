f8b083682523ff3e5be6760bb0732c01e1b2cfed
mariolazzari <mario.lazzari@gmail.com>
2019-02-17 19:43:53 +0100

    Created server

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APPLICATION_ID:
+    "50ec9e00f29c84c99cae4af75818b237d14f89d4ed55d09047237d553ab77db9",
+  SECRET: "de0b833ef05a9911f63b1e729623f4cc7c5e9715073cccab6d8b89bf547af8f9",
+  CALLBACK_URL: "http://localhost:3000"
+};
